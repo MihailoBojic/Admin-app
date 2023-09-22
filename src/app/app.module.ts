a9a667e94a182babe04a852c769c5aae8b79973c
MihailoBojic <mihailo.bojic@digitalcontrol.me>
2023-09-22 10:44:18 +0200

    Drugi commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+
     AppRoutingModule
   ],
   providers: [],
