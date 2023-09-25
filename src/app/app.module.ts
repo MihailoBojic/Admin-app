dbbd31ee4ef8a715d05a657e3600b454ae689d35
b-sosic <brankasosic@digitalcontrol.me>
2023-09-25 11:01:14 +0200

    New changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FormComponent } from './form/form.component';
 import { FormsModule } from '@angular/forms';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { ActivityOfferComponent } from './activity-offer/activity-offer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FormComponent,
-    SidebarComponent
+    SidebarComponent,
+    ActivityOfferComponent
   ],
   imports: [
     BrowserModule,
